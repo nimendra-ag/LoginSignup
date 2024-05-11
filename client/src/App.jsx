2c9e7a63e3aef70848ffde02a888b305400c6460
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-11 16:27:23 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LoginSignup from './Pages/LoginSignup'
+
+function App() {
+
+
+  return (
+    <>
+      <LoginSignup/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Pages/LoginSignup.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const LoginSignup = () => {
+    const [state, setState] = useState("Login");
+  return (
+    <div>LoginSignup</div>
+  )
+}
+
+export default LoginSignup
